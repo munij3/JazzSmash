d26bd0630092629979b0d9851df8f8a02347a623
Juan Muniain Otero <muniainjuan@icloud.com>
2022-04-29 14:21:27 -0500

    Modified API

--- Assets/Scripts/APITest.cs
+++ Assets/Scripts/APITest.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using Unity;
 using UnityEngine;
 using UnityEngine.Networking;
-// using TMPro_Test;
+
 // Allow the class to be extracted from Unity
 // https://stackoverflow.com/questions/40633388/show-members-of-a-class-in-unity3d-inspector
 [System.Serializable]
@@ -24,7 +24,7 @@ public class User_data
 [System.Serializable]
 public class Attempts
 {
-    // public int user_id;
+    public int user_id;
     public string level_att;
     public int score;
     public double accuracy;
@@ -33,11 +33,11 @@ public class Attempts
 }
 
 // Allow the class to be extracted from Unity
-// [System.Serializable]
-// public class User_data_list
-// {
-//     public List<User_data> userData;
-// }
+[System.Serializable]
+public class User_data_list
+{
+    public List<User_data> userData;
+}
 
 public class APITest : MonoBehaviour
 {
@@ -62,30 +62,27 @@ public class APITest : MonoBehaviour
         StartCoroutine(AddAttempt(level_attempted, obtained_score, obtained_accuracy, elapsed_time, obtained_result));
     }
 
-    // CHECAR COMO OBTENER USUARIO PARA VERIFICAR SI YA EXISTE EN LA DATABASE
+    IEnumerator GetUsers(string user_name)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url + getUsersEP);
+        yield return www.SendWebRequest();
 
-    // IEnumerator GetUsers()
-    // {
-    //     UnityWebRequest www = UnityWebRequest.Get(url + getUsersEP);
-    //     yield return www.SendWebRequest();
-
-    //     if (www.result == UnityWebRequest.Result.Success) {
-    //         //Debug.Log("Response: " + www.downloadHandler.text);
-    //         // Compose the response to look like the object we want to extract
-    //         // https://answers.unity.com/questions/1503047/json-must-represent-an-object-type.html
-    //         string jsonString = "{\"users\":" + www.downloadHandler.text + "}";
-    //         allUserData = JsonUtility.FromJson<User_data_list>(jsonString);
-    //         // DisplayUsers();
-    //     } else {
-    //         Debug.Log("Error: " + www.error);
-    //     }
-    // }
+        if (www.result == UnityWebRequest.Result.Success) {
+            //Debug.Log("Response: " + www.downloadHandler.text);
+            // Compose the response to look like the object we want to extract
+            // https://answers.unity.com/questions/1503047/json-must-represent-an-object-type.html
+            string jsonString = "{\"users\":" + www.downloadHandler.text + "}";
+            // user_name = JsonUtility.FromJson<User_data_list>(jsonString)
+        } else {
+            Debug.Log("Error: " + www.error);
+        }
+    }
 
     IEnumerator AddUser(string input_name, string input_country)
     {
-
         // Create the object to be sent as json
         User_data newUser = new User_data();
+        newUser.user_id = GetUsers(user_name);
         newUser.user_name = input_name;
         newUser.country = input_country;
         Debug.Log("USER: " + newUser);
